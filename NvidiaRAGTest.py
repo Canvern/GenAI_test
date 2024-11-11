7388cfc77af27a473f375c37d64e3bf57e6fdf87
Canvern <146818709+Canvern@users.noreply.github.com>
2024-11-11 10:13:11 +0000

    Update NvidiaRAGTest.py

--- NvidiaRAGTest.py
+++ NvidiaRAGTest.py
@@ -1,7 +1,7 @@
 python3 -m virtualenv genai
 source genai/bin/activate
 
-pip install -r community/5_mins_rag_no_gpu/requirements.txt
+#pip install -r community/5_mins_rag_no_gpu/requirements.txt
 
 export NVIDIA_API_KEY="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"
 
