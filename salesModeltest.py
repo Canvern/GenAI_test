2a00d2eee609c9a4f9bb02e5ec8f9754e3d7949d
Canvern <146818709+Canvern@users.noreply.github.com>
2024-11-11 09:59:48 +0000

    FirstStreamlitTest.py

--- salesModeltest.py
+++ salesModeltest.py
@@ -1,9 +1,3 @@
 import streamlit as st
-import mymodel as m
-
-st.write("""
-# Sales model
-Below are our sales predictions for this customer.
-""")
-
-st.write(m.run(window=15))
+st.title("Hi, This is a Test")
+st.text("Further note.")
